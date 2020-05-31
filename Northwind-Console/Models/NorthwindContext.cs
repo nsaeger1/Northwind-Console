e299d3af6078093844e56a69f597a14cb94e5ce8
lawl281 <saegernick@gmail.com>
2020-05-31 14:37:59 -0500

    assign 12

--- Northwind-Console/Models/NorthwindContext.cs
+++ Northwind-Console/Models/NorthwindContext.cs
@@ -4,10 +4,20 @@ namespace NorthwindConsole.Models
 {
     public class NorthwindContext : DbContext
     {
-        public NorthwindContext() : base("name=NorthwindContext") { }
+        public NorthwindContext() : base("name=NorthwindContext")
+        {
+        }
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Supplier> Suppliers { get; set; }
+
+        public void AddCategory(Category category)
+        {
+            this.Categories.Add(category);
+            this.SaveChanges();
+        }
     }
+
+    
 }
